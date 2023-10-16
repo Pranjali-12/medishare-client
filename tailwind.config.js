4a2fc177f32334f75fe3db24e732f2c621feb192
Sagar Chavan <sagarchavan25889@gmail.com>
2023-10-16 12:48:20 +0530

    UploadMedicine Page

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,13 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        custom: ['Montserrat', 'sans-serif'],
+        spartan: ['Spartan','sans-serif'],
+      },
+    },
   },
-  plugins: [],
+  plugins: [],
 }
 
