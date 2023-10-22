78e316b599bce8afa6c3c764283b84c49008b028
harshali <harshalifarde@gmail.com>
2023-10-22 11:24:50 +0530

    home css added

--- src/Components/SignInLeft.jsx
+++ src/Components/SignInLeft.jsx
@@ -1,102 +1,107 @@
-import React from 'react'
-import '../styles/SignInLeft.css'
+import React from "react";
+import "../styles/SignInLeft.css";
 
 const SignInLeft = () => {
   return (
-    <div className='flex flex-row flex-wrap ' >
-        <div className='sm:w-1/2   '  style={{backgroundColor:"#1CB5BD"}}>
-           <img src={require("../assets/signup.png")} className='sign-img '></img>
-        </div>
+    <div className="flex flex-row flex-wrap ">
+      <div className="sm:w-1/2   " style={{ backgroundColor: "#1CB5BD" }}>
+        <img src={require("../assets/signup.png")} className="sign-img "></img>
+      </div>
 
-
-        <div className='sm:w-1/2 flex justify-center'>
+      <div className="sm:w-1/2 flex justify-center">
         <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-          
-          <h2 className="mt-5 text-left text-5xl font-bold leading-9 tracking-tight text-gray-900">
-            Welcome
-          </h2>
-        </div>
+          <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+            <h2 className="mt-5 text-left text-5xl font-bold leading-9 tracking-tight text-gray-900">
+              Welcome
+            </h2>
+          </div>
 
-        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-          <form className="space-y-6" action="#" method="POST">
-          
-            <div>
-              <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
-                Email address
-              </label>
-              <div className="mt-2">
-                <input
-                  id="email"
-                  name="email"
-                  type="email"
-                  autoComplete="email"
-                  placeholder='Enter Your Address'
-                  required
-                  className="block w-full text-sm font-semibold border-b  py-1.5 text-gray-900   placeholder:text-gray-400 outline-none" style={{borderColor:"#98B3D6"}}
-                />
-              </div>
-            </div>
-
-            <div>
-              <div className="flex items-center justify-between">
-                <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
-                  Password
+          <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+            <form className="space-y-6" action="#" method="POST">
+              <div>
+                <label
+                  htmlFor="email"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Email address
                 </label>
-               
-              </div>
-              <div className="mt-2">
-                <input
-                  id="password"
-                  name="password"
-                  type="password"
-                  autoComplete="current-password"
-                  placeholder='********'
-                  required
-                  className="block w-full text-sm font-semibold  border-b  py-1.5 text-gray-900   placeholder:text-gray-400 outline-none" style={{borderColor:"#98B3D6"}}
-                />
-              </div>
-            </div>
-            <div className="text-sm flex justify-between">
-            <div className=''>
-                <label className="flex block w-full text-sm font-bold  text-gray-500 outline-none"
->
-  <input type="checkbox" /> 
-  <h6 className='mx-2'> Remember me</h6>
-</label>
-</div>
-
-                  <a href="#" className="text-sm font-bold hover:text-teal-800 text-teal-600">
-                    Forgot password?
-                  </a>
-
-
+                <div className="mt-2">
+                  <input
+                    id="email"
+                    name="email"
+                    type="email"
+                    autoComplete="email"
+                    placeholder="Enter Your Address"
+                    required
+                    className="block w-full text-sm font-semibold border-b  py-1.5 text-gray-900   placeholder:text-gray-400 outline-none"
+                    style={{ borderColor: "#98B3D6" }}
+                  />
                 </div>
-            <div>
-              <button
-                type="submit"
-                className="flex w-full justify-center rounded-full  px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm  focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 " style={{backgroundColor:"#1CB5BD"}}
-              >
-                Login
-              </button>
-            </div>
-          </form>
+              </div>
 
-          <p className="mt-2 text-center font-bold py-4 text-sm text-gray-500">
-            New member?{' '}
-            <a href="/signup" className="font-semibold leading-6  cursor-pointer" style={{ color: "#1CB5BD" }}>
-              Sign Up
-            </a>
-          </p>
+              <div>
+                <div className="flex items-center justify-between">
+                  <label
+                    htmlFor="password"
+                    className="block text-sm font-medium leading-6 text-gray-900"
+                  >
+                    Password
+                  </label>
+                </div>
+                <div className="mt-2">
+                  <input
+                    id="password"
+                    name="password"
+                    type="password"
+                    autoComplete="current-password"
+                    placeholder="********"
+                    required
+                    className="block w-full text-sm font-semibold  border-b  py-1.5 text-gray-900   placeholder:text-gray-400 outline-none"
+                    style={{ borderColor: "#98B3D6" }}
+                  />
+                </div>
+              </div>
+              <div className="text-sm flex justify-between">
+                <div className="">
+                  <label className="flex block w-full text-sm font-bold  text-gray-500 outline-none">
+                    <input type="checkbox" />
+                    <h6 className="mx-2"> Remember me</h6>
+                  </label>
+                </div>
+
+                <a
+                  href="#"
+                  className="text-sm font-bold hover:text-teal-800 text-teal-600"
+                >
+                  Forgot password?
+                </a>
+              </div>
+              <div>
+                <button
+                  type="submit"
+                  className="flex w-full justify-center rounded-full  px-3 py-1.5 text-lg font-bold leading-6 text-white shadow-sm  focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 "
+                  style={{ backgroundColor: "#1CB5BD" }}
+                >
+                  Login
+                </button>
+              </div>
+            </form>
+
+            <p className="mt-2 text-center font-bold py-4 text-sm text-gray-500">
+              New member?{" "}
+              <a
+                href="/signup"
+                className="font-semibold leading-6  cursor-pointer"
+                style={{ color: "#1CB5BD" }}
+              >
+                Sign Up
+              </a>
+            </p>
+          </div>
         </div>
       </div>
-        </div>
-
-
     </div>
-  )
-}
+  );
+};
 
-export default SignInLeft
-
-
+export default SignInLeft;
--- src/Components/SignUpCon.jsx
+++ src/Components/SignUpCon.jsx
@@ -1,7 +1,6 @@
-
-import '../styles/SignInLeft.css'
-import React, { useState } from 'react';
-import {FaEyeSlash,FaEye} from 'react-icons/fa';
+import "../styles/SignInLeft.css";
+import React, { useState } from "react";
+import { FaEyeSlash, FaEye } from "react-icons/fa";
 
 const SignUpCon = () => {
   const [showPassword, setShowPassword] = useState(false);
@@ -12,12 +11,16 @@ const SignUpCon = () => {
 
   return (
     <div>
-      <div className='flex flex-row flex-wrap '>
-        <div className='sm:w-1/2 ' style={{ backgroundColor: "#1CB5BD" }}>
-          <img src={require("../assets/signup.png")} className='sign-img ' alt="Signup" />
+      <div className="flex flex-row flex-wrap ">
+        <div className="sm:w-1/2 " style={{ backgroundColor: "#1CB5BD" }}>
+          <img
+            src={require("../assets/signup.png")}
+            className="sign-img "
+            alt="Signup"
+          />
         </div>
 
-        <div className='sm:w-1/2 flex justify-center'>
+        <div className="sm:w-1/2 flex justify-center">
           <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
             <div className="sm:mx-auto sm:w-full sm:max-w-sm">
               <h2 className="mt-5 text-left text-5xl font-bold leading-9 tracking-tight text-gray-900">
@@ -27,9 +30,12 @@ const SignUpCon = () => {
 
             <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
               <form className="space-y-6" action="#" method="POST">
-                <div className='flex sm:flex flex:nowrap sm:wrap'>
+                <div className="flex sm:flex flex:nowrap sm:wrap">
                   <div>
-                    <label htmlFor="email" className="block text-sm font-bold leading-6 text-gray-900">
+                    <label
+                      htmlFor="email"
+                      className="block text-sm font-bold leading-6 text-gray-900"
+                    >
                       First Name
                     </label>
                     <div className="mt-2 me-5">
@@ -38,7 +44,7 @@ const SignUpCon = () => {
                         name="fname"
                         type="text"
                         autoComplete="fname"
-                        placeholder='Enter First Name'
+                        placeholder="Enter First Name"
                         required
                         className="block w-full text-sm font-semibold border-b py-1.5 text-gray-900 placeholder:text-gray-400 outline-none"
                         style={{ borderColor: "#98B3D6" }}
@@ -46,7 +52,10 @@ const SignUpCon = () => {
                     </div>
                   </div>
                   <div>
-                    <label htmlFor="email" className="block text-sm font-bold leading-6 text-gray-900">
+                    <label
+                      htmlFor="email"
+                      className="block text-sm font-bold leading-6 text-gray-900"
+                    >
                       Last Name
                     </label>
                     <div className="mt-2">
@@ -55,7 +64,7 @@ const SignUpCon = () => {
                         name="lname"
                         type="text"
                         autoComplete="lname"
-                        placeholder='Enter Last Name'
+                        placeholder="Enter Last Name"
                         required
                         className="block w-full text-sm font-semibold border-b py-1.5 text-gray-900 placeholder:text-gray-400 outline-none"
                         style={{ borderColor: "#98B3D6" }}
@@ -64,7 +73,10 @@ const SignUpCon = () => {
                   </div>
                 </div>
                 <div>
-                  <label htmlFor="email" className="block text-sm font-bold leading-6 text-gray-900">
+                  <label
+                    htmlFor="email"
+                    className="block text-sm font-bold leading-6 text-gray-900"
+                  >
                     Email address
                   </label>
                   <div className="mt-2 ">
@@ -73,7 +85,7 @@ const SignUpCon = () => {
                       name="email"
                       type="email"
                       autoComplete="email"
-                      placeholder='Enter Your Address'
+                      placeholder="Enter Your Address"
                       required
                       className="block w-full text-sm font-semibold border-b py-1.5 text-gray-900 placeholder:text-gray-400 outline-none"
                       style={{ borderColor: "#98B3D6" }}
@@ -82,7 +94,10 @@ const SignUpCon = () => {
                 </div>
                 <div>
                   <div className="flex items-center justify-between">
-                    <label htmlFor="password" className="block text-sm font-bold leading-6 text-gray-900">
+                    <label
+                      htmlFor="password"
+                      className="block text-sm font-bold leading-6 text-gray-900"
+                    >
                       Create Password
                     </label>
                   </div>
@@ -93,7 +108,7 @@ const SignUpCon = () => {
                         name="password"
                         type={showPassword ? "text" : "password"} // Toggle between "text" and "password"
                         autoComplete="current-password"
-                        placeholder='Enter Password'
+                        placeholder="Enter Password"
                         required
                         className="block w-full text-sm font-semibold border-b py-1.5 text-gray-900 placeholder:text-gray-400 outline-none"
                         style={{ borderColor: "#98B3D6" }}
@@ -102,27 +117,29 @@ const SignUpCon = () => {
                         className="absolute right-3 top-2 cursor-pointer"
                         onClick={togglePasswordVisibility}
                       >
-                         {showPassword ? (
-                 <FaEyeSlash></FaEyeSlash>
-                ) : (
-                  <FaEye></FaEye>
-                )}
+                        {showPassword ? (
+                          <FaEyeSlash></FaEyeSlash>
+                        ) : (
+                          <FaEye></FaEye>
+                        )}
                       </span>
                     </div>
                   </div>
                 </div>
-                <div className='mt-2'>
-                <label className="flex block w-full text-xs font-bold  text-gray-500 outline-none"
->
-  <input type="checkbox" /> 
-  <h6 className='mx-2'> By signing up, you agree to our Terms & Conditions</h6>
-</label>
-</div>
+                <div className="mt-2">
+                  <label className="flex block w-full text-xs font-bold  text-gray-500 outline-none">
+                    <input type="checkbox" />
+                    <h6 className="mx-2">
+                      {" "}
+                      By signing up, you agree to our Terms & Conditions
+                    </h6>
+                  </label>
+                </div>
 
                 <div>
                   <button
                     type="submit"
-                    className="flex w-full justify-center rounded-full px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2"
+                    className="flex w-full justify-center rounded-full px-3 py-1.5 text-lg font-bold leading-6 text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2"
                     style={{ backgroundColor: "#1CB5BD" }}
                   >
                     Sign Up
@@ -131,12 +148,14 @@ const SignUpCon = () => {
               </form>
 
               <p className="mt-2 text-center font-bold py-4 text-sm text-gray-500">
-                Already a member?{' '}
-             
-                <a href='/signin' className="font-semibold leading-6  cursor-pointer" style={{ color: "#1CB5BD" }}>
+                Already a member?{" "}
+                <a
+                  href="/signin"
+                  className="font-semibold leading-6  cursor-pointer"
+                  style={{ color: "#1CB5BD" }}
+                >
                   Login
                 </a>
-               
               </p>
             </div>
           </div>
@@ -144,6 +163,6 @@ const SignUpCon = () => {
       </div>
     </div>
   );
-}
+};
 
 export default SignUpCon;
