3e0c7bad1b434a70bf568b03ad2ed85cae868d79
Pranjali-12 <kasbalepranjali@gmail.com>
2024-01-28 02:52:05 +0530

    admin accept request done

---
+++ src/Components/AcceptComponent.jsx
@@ -0,0 +1,102 @@
+import React, { useState } from 'react'
+import moment from 'moment'
+import axios from 'axios'
+import { ImCheckmark } from "react-icons/im";
+import { ImCross } from "react-icons/im";
+
+const AcceptComponent = (props) => {
+
+    console.log(props.id)
+
+    const formatDate = (dateString) => {
+        const date = new Date(dateString);
+        const day = date.getUTCDate();
+        const month = date.getUTCMonth() + 1;
+        const year = date.getUTCFullYear();
+
+        const formattedDay = day < 10 ? `0${day}` : `${day}`;
+        const formattedMonth = month < 10 ? `0${month}` : `${month}`;
+
+        return `${formattedDay}/${formattedMonth}/${year}`;
+    };
+
+    const formattedManufacturedDate = formatDate(props.mfDate);
+    const formattedExpiryDate = formatDate(props.expiryDate);
+
+    const handleAccept = () => {
+        axios.post(`http://localhost:2000/admin/accept?id=${props.id}`)
+            .then(res => {
+                console.log(res.data)
+                window.location.reload()
+            }).catch(error => {
+                console.log(error);
+            })
+    };
+
+    const handleReject = () => {
+        axios.post(`http://localhost:2000/admin/reject?id=${props.id}`)
+            .then(res => {
+                console.log(res.data)
+                window.location.reload()
+            }).catch(error => {
+                console.log(error);
+            })
+    };
+
+    return (
+        <>
+            {/* <AcceptComponent id={item.id} donorName={item.name} receiverName={item.illness} medicineName={item.owner} mfDate={item.mf_date} expiryDate={item.exp_date} quantity={item.quantity} req_quantity={item.quantity} appStatus={item.accept} createdAt={item.createdAt} /> */}
+            <tr class="bg-white border-b text-zinc-800 text-center font-semibold">
+                <td scope="col" class="px-6 py-3">
+                    {props.donorName}
+                </td>
+                <td scope="col" class="px-6 py-3">
+                    {props.receiverName}
+                </td>
+                <td scope="col" class="px-6 py-3">
+                    {props.medicineName}
+                </td>
+                <td scope="col" class="px-6 py-3">
+                    {formattedManufacturedDate}
+                </td>
+                <td scope="col" class="px-6 py-3">
+                    {formattedExpiryDate}
+                </td>
+                <td scope="col" class="px-6 py-3">
+                    {props.quantity}
+                </td>
+                <td scope="col" class="px-6 py-3">
+                    {props.req_quantity}
+                </td>
+                <td scope="col" class="px-6 py-3">
+                    {/* {props.appStatus} */}
+                    {
+                        props.appStatus == "accept" ?
+                            (
+                                <div className='px-2 py-1 text-white text-sm bg-green-500 rounded-md font-bold'>Accept</div>
+                            ) : (
+
+                                props.appStatus == "reject" ?
+                                    (
+                                        <div className='px-2 py-1 text-white text-sm bg-red-500 rounded-md font-bold'>Reject</div>
+                                    )
+                                    : (
+                                        <div className='flex gap-2 items-center'>
+                                            Pending
+                                            <div className='border-2 p-1 hover:cursor-pointer hover:bg-gray-200 rounded-md' onClick={handleAccept}><ImCheckmark size={24} color='green'/></div>
+                                            <div className='border-2 p-1 hover:cursor-pointer hover:bg-gray-200 rounded-md' onClick={handleReject}><ImCross size={20}color='red'/></div>
+                                        </div>
+                                    )
+
+                            )
+                    }
+                </td>
+
+                <td class="px-6 py-4 font-light">
+                    {moment(props.createdAt).fromNow()}
+                </td>
+            </tr></>
+    )
+}
+
+export default AcceptComponent
--- src/pages/AdminAccept.jsx
+++ src/pages/AdminAccept.jsx
@@ -1,8 +1,71 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
+import AcceptComponent from '../Components/AcceptComponent';
 
 const AdminAccept = () => {
+  const [req,setReq]=useState([]);
+  useEffect(() => {
+    axios.get(`http://localhost:2000/admin/getallreq`)
+      .then(res => {
+        console.log(res.data.reqs)
+        setReq(res.data.reqs);
+
+      }).catch(error => {
+        console.log(error);
+      })
+  }, [])
   return (
-    <div>AdminAccept</div>
+    <div>
+            
+            <div className='mb-6 text-3xl text-center font-bold text-black'>
+                Medicine Request List
+            </div>
+
+
+            <div class="mx-5 mb-4 relative overflow-x-auto shadow-md">
+                <table class="w-full text-sm text-left rtl:text-right text-[#49bbc1]">
+                    <thead class="text-[#62c9cf] text-xl text-center">
+                        <tr>
+                            <th scope="col" class="px-6 py-3">
+                                Donor name
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Receiver Name
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Medicine Name
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Manufactured Date
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Expiry Date
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Medicine Quantity
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Requested Quantity
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Approval
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Requested At
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            req && req.map((item)=>(
+                                <AcceptComponent id={item._id} donorName={item.medicine.donor.firstName+" "+item.medicine.donor.lastName} receiverName={item.receiver.name} medicineName={item.medicine.name} mfDate={item.medicine.mf_date} expiryDate={item.medicine.exp_date} quantity={item.medicine.quantity} req_quantity={item.quantity} appStatus={item.accept} createdAt={item.createdAt} />
+                            ))
+                        }
+                    </tbody>
+                </table>
+            </div>
+
+        </div>
   )
 }
 
